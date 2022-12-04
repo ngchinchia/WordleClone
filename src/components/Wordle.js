6445894a4c0b90c3652f63626e3ba53584a716a8
Ng Chin Chia <billyngchinchia@gmail.com>
2022-12-04 17:34:55 +0800

    comments

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,18 +1,21 @@
-import React, { useEffect } from 'react'
-import useWordle from '../hooks/useWordle'
+import React, { useEffect } from "react";
+import useWordle from "../hooks/useWordle";
 
 export default function Wordle({ solution }) {
-  const { currentGuess, handleKeyup } = useWordle(solution)
+  /* Grabbing things from useWorld.js */
+  const { currentGuess, handleKeyup } = useWordle(solution);
 
   useEffect(() => {
-    window.addEventListener('keyup', handleKeyup)
+    /* everytime user presses a key and release, fire this function */
+    window.addEventListener("keyup", handleKeyup);
 
-    return () => window.removeEventListener('keyup', handleKeyup)
-  }, [handleKeyup])
+    /* cleanup function to unattach event */
+    return () => window.removeEventListener("keyup", handleKeyup);
+  }, [handleKeyup]);
 
   return (
     <div>
       <div>Current Guess - {currentGuess}</div>
     </div>
-  )
+  );
 }
