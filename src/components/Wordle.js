67a9741459848da45b4110b6914d16d0b7f26626
Ng Chin Chia <billyngchinchia@gmail.com>
2022-12-05 16:01:17 +0800

    keypad

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import useWordle from "../hooks/useWordle";
+import Grid from "./Grid";
+import Keypad from "./Keypad";
 
 export default function Wordle({ solution }) {
   /* Grabbing things from useWorld.js */
-  const { currentGuess, handleKeyup } = useWordle(solution);
+  const { currentGuess, handleKeyup, guesses, isCorrect, turn } = useWordle(solution);
 
   useEffect(() => {
     /* everytime user presses a key and release, fire this function */
@@ -13,10 +15,16 @@ export default function Wordle({ solution }) {
     return () => window.removeEventListener("keyup", handleKeyup);
   }, [handleKeyup]);
 
+  useEffect(() => {
+    console.log(guesses, turn, isCorrect)
+  }, [guesses, turn, isCorrect])
+
   return (
     <div>
       <div>solution - {solution}</div>
       <div>Current Guess - {currentGuess}</div>
+      <Grid currentGuess = {currentGuess} guesses = {guesses} turn = {turn} />
+      <Keypad />
     </div>
   );
 }
--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -3,68 +3,82 @@ import { useState } from "react";
 const useWordle = (solution) => {
   const [turn, setTurn] = useState(0); // Tracks what turn the user is on, after 6 guesses, it is game over.
   const [currentGuess, setCurrentGuess] = useState(""); // Tracking what the user is currently typing in current guess and update.
-  const [guesses, setGuesses] = useState([]); // each guess is an array
+  const [guesses, setGuesses] = useState([...Array(6)]); // each guess is an array
   const [history, setHistory] = useState([]); // each guess is a string
   const [isCorrect, setIsCorrect] = useState(false); //Changes true only when user wins game.
 
   // format a guess into an array of letter objects
   // e.g. [{key: 'a', color: 'yellow'}]
   const formatGuess = () => {
-    let solutionArray = [...solution] 
+    let solutionArray = [...solution]; // Gets copy of solution as an array
     let formattedGuess = [...currentGuess].map((l) => {
-      return {key: l, color: 'grey'}
-    })
+      // Gets copy of user's guess as an array and map each individual letter
+      return { key: l, color: "grey" };
+    });
 
     // Check for green letters
     formattedGuess.forEach((l, i) => {
-      if (solutionArray[i] === l.key){
-        formattedGuess[i].color = 'green'
-        solutionArray[i] = null
+      // forEach letter and index
+      if (solutionArray[i] === l.key) {
+        formattedGuess[i].color = "green";
+        solutionArray[i] = null;
       }
-    })
+    });
 
     // Check for yellow letters
     formattedGuess.forEach((l, i) => {
-      if (solutionArray.includes(l.key) && l.color !== 'green'){
-        formattedGuess[i].color = 'yellow'
-        solutionArray[solutionArray.indexOf(l.key)] = null // find index of particular letter in solution
-
+      if (solutionArray.includes(l.key) && l.color !== "green") {
+        formattedGuess[i].color = "yellow";
+        solutionArray[solutionArray.indexOf(l.key)] = null; // find index of particular letter in solution
       }
-    })
+    });
 
-    return formattedGuess
-
+    return formattedGuess;
   };
 
   // add a new guess to the guesses state
   // update the isCorrect state if the guess is correct
   // add one to the turn state
-  const addNewGuess = () => {};
+  const addNewGuess = (formattedGuess) => {
+    if (currentGuess === solution) {
+      setIsCorrect(true);
+    }
+    setGuesses((prevGuesses) => {
+      let newGuesses = [...prevGuesses];
+      newGuesses[turn] = formattedGuess;
+      return newGuesses;
+    });
+    setHistory((prevHistory) => {
+      return [...prevHistory, currentGuess]; // Takes currentGuess and store as string format
+    });
+    setTurn((prevTurn) => {
+      // Increment starting turn
+      return prevTurn + 1;
+    });
+    setCurrentGuess(""); // Clears current guess once it had a guess and start on a new row with blank slate
+  };
 
   // handle keyup event & track current guess
   // if user presses enter, add the new guess
   const handleKeyup = ({ key }) => {
-    
-    if (key === 'Enter')
-    {
+    if (key === "Enter") {
       // only add guess if turn is < 5
-      if (turn > 5){
-        console.log('you used all guesses')
-        return
+      if (turn > 5) {
+        console.log("you used all guesses");
+        return;
       }
       // do not allow duplicate words
-      if (history.includes(currentGuess)){
-        console.log('you already tried that word')
-        return
+      if (history.includes(currentGuess)) {
+        console.log("you already tried that word");
+        return;
       }
       // check word is 5 chars long
-      if (currentGuess.length !== 5){
-        console.log('word must be 5 chars long')
-        return
+      if (currentGuess.length !== 5) {
+        console.log("word must be 5 chars long");
+        return;
       }
-      const formatted = formatGuess()
-      console.log(formatted)
-
+      const formatted = formatGuess();
+      addNewGuess(formatted);
     }
 
     /* Allows deletion of letter */
